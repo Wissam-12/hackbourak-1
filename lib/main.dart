849f79829261f3d2faad02bb32296cb45724fe8c
doudou820014 <jw_azizi@esi.dz>
2022-04-06 03:22:05 +0100

    done ??

--- lib/main.dart
+++ lib/main.dart
@@ -54,15 +54,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
        
-            headline1: TextStyle(fontSize: 22.0, color: Color(0xFFE32929),),
-            headline2: TextStyle(fontSize: 36.0,
-              fontWeight: FontWeight.w700,
-              color: Color(0xFF343434),),
-            bodyText1: TextStyle(fontSize: 14.0,
-              fontWeight: FontWeight.w400,
-              color: Color(0xFFE32929),),
-          ),
-        ),
+
 
         home: FutureBuilder(
           future: Firebase.initializeApp(
@@ -74,8 +66,9 @@ class MyApp extends StatelessWidget {
             }
             return SplashScreen();
           },
-        ));
+        )
     );
+
   }
 }
 

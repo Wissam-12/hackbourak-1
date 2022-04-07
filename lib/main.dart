9b242ddcaff8ef8eaabc0e372631e5da25e915d3
NassimAm <nassimam2003@gmail.com>
2022-04-07 16:24:08 +0100

    Loading Fix

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
           ),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return PopUp();
+              return Loading();
             }
             return SplashScreen();
           },
--- lib/screens/Loading.dart
+++ lib/screens/Loading.dart
@@ -11,7 +11,7 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFE32929),
+      backgroundColor: Colors.white,
       body: Container(
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
@@ -24,59 +24,55 @@ class _LoadingState extends State<Loading> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Image.asset('assets/img/ftournet.png'),
+            // Container(
+            //   margin: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 0),
+            //   child: const Text(
+            //     'Bienvenue !',
+            //     style: TextStyle(
+            //         color: Color(0xFF343434),
+            //         fontWeight: FontWeight.w700,
+            //         fontSize: 46,
+            //         fontFamily: 'Poppins'),
+            //   ),
+            // ),
             Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(90.0),
+              margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 0, horizontal: 3.0),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFE32929),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 0, horizontal: 3.0),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(184, 227, 41, 41),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 0, horizontal: 3.0),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(84, 227, 41, 41),
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                  )
+                ],
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 0),
-              child: const Text(
-                'Bienvenue !',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 46,
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 3.0),
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(90.0),
-                  ),
-                ),
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 3.0),
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(119, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(90.0),
-                  ),
-                ),
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 3.0),
-                  width: 10,
-                  height: 10,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(57, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(90.0),
-                  ),
-                )
-              ],
             )
           ],
         ),
